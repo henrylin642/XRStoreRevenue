1387b51704e9659be18989b08a980f3479486bb8
henry642 <henry642@gmail.com>
2026-02-02 19:24:56 +0800

    chore: remove legacy operations exel importer

--- src/components/dashboard-view.tsx
+++ src/components/dashboard-view.tsx
@@ -18,7 +18,6 @@ import { getVisitorStats, getDailyVisitorStats } from '@/lib/visitor-data';
 import { HOLIDAY_DATA_2026, getDailyRemark, isPublicHoliday } from '@/lib/holiday-data-2026';
 import { updateDailyVisitorCount } from '@/app/actions/visitor-actions';
 import { getSystemConfig, updateSystemConfig, getSystemConfigsByPattern } from '@/app/actions/config-actions';
-import { OperationsExcelImporter } from './operations-excel-importer';
 import { BackupManager } from './backup-manager';
 import { AiCfoChat } from './ai-cfo-chat';
 
@@ -2263,7 +2262,6 @@ export default function DashboardView({ transactions, session }: DashboardViewPr
                             ))}
                         </select>
 
-                        <OperationsExcelImporter />
                         <BackupManager />
                         <UploadButton />
 
